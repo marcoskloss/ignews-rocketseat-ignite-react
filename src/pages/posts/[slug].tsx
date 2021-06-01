d87678165e1971c12093c7a23d29bb83364223ef
marcoskloss <marcoskloss2001@gmail.com>
2021-06-01 17:34:44 -0300

    add post preview page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
@@ -65,5 +65,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     }),
   };
 
-  return {props: { post }}
+  return {
+    props: { post },
+  }
 };
